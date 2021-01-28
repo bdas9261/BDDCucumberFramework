678362496ca3b715b06288e777d110d64d1512d9
biswa <bdas9261@gmail.com>
2021-01-28 21:36:07 +0530

    commit TestScript3 class

---
+++ src/main/java/com/gcr/config/TestScript3.java
@@ -0,0 +1,16 @@
+package com.gcr.config;
+
+public class TestScript3 {
+	public void m1() {
+		  
+	  }
+	public void m2() {
+		  
+	  }
+	public void m3() {
+		  
+	}
+	public void m4() {
+		  
+	}
+}
